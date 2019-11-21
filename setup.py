6fc69a031693c47243639965e4443c4ddd9292aa
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2019-11-21 09:11:05 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='mining-ROI',
     version='0.0.1',
-    packages=['roi_miner'],
+    packages=['roi_miner.miner', 'roi_miner.miner.grid_miner'],
     author='Dubray Alexandre',
-    install_requres=['gurobipy'],
+    install_requires=['gurobipy'],
     description='Mining Regions of Interest on trajectory data'
 )
