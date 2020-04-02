f5842d188d9b903329183dc5ab725d593649f142
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2020-04-02 20:27:44 +0200

    add shapes module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='mining-ROI',
     version='0.0.1',
-    packages=['roi_miner', 'roi_miner.grid_miners', 'roi_miner.grid_miners.MDL_miner'],
+    packages=['roi_miner', 'roi_miner.grid_miners', 'roi_miner.grid_miners.MDL_miner', 'roi_miner.grid_miners.shapes'],
     author='Dubray Alexandre',
     install_requires=['gurobipy'],
     description='Mining constrained Regions of Interest on trajectory data'
